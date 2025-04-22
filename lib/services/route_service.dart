74b73c2779ef9364697fe33e1931240f55039030
LeandroIV <gica.leandroiv@gmail.com>
2025-04-22 20:37:00 +0800

    modified logo and routes jip

--- lib/services/route_service.dart
+++ lib/services/route_service.dart
@@ -147,7 +147,7 @@ class RouteService {
     return [
       PUVRoute(
         id: 'r2',
-        name: 'R2 - Carmen to Divisoria',
+        name: 'R2 - Gaisano-Agora-Cogon-Carmen',
         description: 'Route from Carmen to Divisoria via Corrales Avenue',
         puvType: 'Jeepney',
         routeCode: 'R2',
@@ -167,65 +167,85 @@ class RouteService {
           const LatLng(8.486158, 124.638827), // liceo
           const LatLng(8.486261, 124.649210), // gaisano
         ],
-        startPointName: 'Carmen Terminal',
-        endPointName: 'Divisoria',
+        startPointName: 'Gaisano',
+        endPointName: 'Carmen',
         estimatedTravelTime: 25,
         farePrice: 12.0,
         colorValue: 0xFFFF6D00, // Deep Orange
         isActive: true,
       ),
       PUVRoute(
-        id: 'r3',
-        name: 'R3 - Bulua to Divisoria',
-        description: 'Route from Bulua to Divisoria via JR Borja Street',
+        id: 'C2',
+        name: 'C2 - Patag-Gaisano-Limketkai-Cogon',
+        description: 'Route from Patag to Cogon via Gaisano',
         puvType: 'Jeepney',
-        routeCode: 'R3',
+        routeCode: 'C2',
         waypoints: [
-          const LatLng(8.5012, 124.6297), // Bulua
-          const LatLng(8.4941, 124.6344), // Point 1
-          const LatLng(8.4872, 124.6397), // Point 2
-          const LatLng(8.4792, 124.6465), // Divisoria
+          const LatLng(8.477434, 124.649630), // Cogon
+          const LatLng(8.476343, 124.639981), // ysalina bridge
+          const LatLng(8.480251, 124.637131), // carmen cogon
+          const LatLng(8.485040, 124.637276), // mango st
+          const LatLng(8.487765, 124.626766), // Patag
+          const LatLng(8.486605, 124.638888), // lieo
+          const LatLng(8.486261, 124.649210), // gaisano
+          const LatLng(8.477434, 124.649630), // Cogon
         ],
-        startPointName: 'Bulua Terminal',
-        endPointName: 'Divisoria',
+        startPointName: 'Patag',
+        endPointName: 'Cogon',
         estimatedTravelTime: 30,
         farePrice: 13.0,
         colorValue: 0xFF2196F3, // Blue
         isActive: true,
       ),
       PUVRoute(
-        id: 'r4',
-        name: 'R4 - Bugo to Lapasan',
-        description: 'Route from Bugo to Lapasan via Highway',
+        id: 'RA',
+        name: 'RA - Pier-Gaisano-Ayala-Cogon',
+        description: 'Route from Pier to Cogon via Gaisano',
         puvType: 'Jeepney',
-        routeCode: 'R4',
+        routeCode: 'RA',
         waypoints: [
-          const LatLng(8.5092, 124.7525), // Bugo
-          const LatLng(8.4989, 124.7301), // Point 1
-          const LatLng(8.4861, 124.6831), // Point 2
-          const LatLng(8.4826, 124.6596), // Lapasan
+          const LatLng(8.486684, 124.650807), // Gaisano main
+          const LatLng(8.498177, 124.660786), // Pier
+          const LatLng(8.504380, 124.661618), // Macabalan Edge
+          const LatLng(8.503708, 124.659001), // Macabalan
+          const LatLng(8.498178, 124.660057), // Juliu Pacana St
+          const LatLng(8.476927, 124.644083), // Divisoria Plaza
+          const LatLng(8.476425, 124.645800), // Xavier
+          const LatLng(8.476817, 124.652773), // borja st
+          const LatLng(8.477448, 124.652930), // Roxas St
+          const LatLng(8.477855, 124.651483), // yacapin to vicente
+          const LatLng(8.480664, 124.650289), // Ebarle st
+          const LatLng(8.485169, 124.650207), // Ayala
+          const LatLng(8.486684, 124.650807), // Gaisano main
         ],
-        startPointName: 'Bugo Terminal',
-        endPointName: 'Lapasan',
+        startPointName: 'Pier',
+        endPointName: 'Cogon',
         estimatedTravelTime: 45,
         farePrice: 15.0,
         colorValue: 0xFF4CAF50, // Green
         isActive: true,
       ),
       PUVRoute(
-        id: 'r10',
-        name: 'R10 - Canitoan to Cogon',
-        description: 'Route from Canitoan to Cogon Market via Highway',
+        id: 'RD',
+        name: 'RD - Gusa-Cugman-Cogon-Limketkai',
+        description: 'Route from Cugman to Limketkai via Gusa',
         puvType: 'Jeepney',
-        routeCode: 'R10',
+        routeCode: 'RD',
         waypoints: [
-          const LatLng(8.4483, 124.5975), // Canitoan
-          const LatLng(8.4561, 124.6185), // Point 1
-          const LatLng(8.4695, 124.6351), // Point 2
-          const LatLng(8.4847, 124.6472), // Cogon Market
+          const LatLng(8.469899, 124.705196), // cugman
+          const LatLng(8.477536, 124.676559), // Gusa
+          const LatLng(8.486028, 124.650684), // Gaisano
+          const LatLng(8.485010, 124.647179), // Velez
+          const LatLng(8.485627, 124.646200), // capistrano
+          const LatLng(8.477565, 124.642297), // Divisoria
+          const LatLng(8.476425, 124.645800), // Xavier
+          const LatLng(8.476817, 124.652773), // borja
+          const LatLng(8.477595, 124.653591), // yacapin
+          const LatLng(8.484484, 124.657109), // ketkai
+          const LatLng(8.469899, 124.705196), // cugman
         ],
-        startPointName: 'Canitoan',
-        endPointName: 'Cogon Market',
+        startPointName: 'Cugman',
+        endPointName: 'Limketkai',
         estimatedTravelTime: 35,
         farePrice: 14.0,
         colorValue: 0xFFE91E63, // Pink
